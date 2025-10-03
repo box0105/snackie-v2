bc64646bea8702d1d4bc22c9444999fb78a23b51
box <timliu105@gmail.com>
2025-10-03 11:36:55 +0800

    Github Actions fix-5 baseHref

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  providers: [{ provide: APP_BASE_HREF, useValue: environment.baseHref }],
 })
 export class App {
   protected readonly title = signal('snackie');
